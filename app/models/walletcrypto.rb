75a7189b10a0dad9f939ac5c1892f2ce3d28d0b6
Eva Reitzel <evareitzel@gmail.com>
2023-05-22 05:08:13 -0400

    quantity col lives in cryptos, now to set up w init value of 1

--- app/models/walletcrypto.rb
+++ app/models/walletcrypto.rb
@@ -1,6 +1,11 @@
 class Walletcrypto < ApplicationRecord
   belongs_to :wallet
   belongs_to :crypto
+  after initialize :init # 
+
+  def init
+    self.crypto.quantity ||= 1
+  end
 
 
 end
--- db/migrate/20230522090145_add_quantity_to_walletcryptos.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToWalletcryptos < ActiveRecord::Migration[6.1]
-  def change
-    add_column :walletcryptos, :quantity, :integer
-  end
-end
