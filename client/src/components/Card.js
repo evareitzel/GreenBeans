b77fabef20da8714ef8d5eb434cdf8e5f457c58e
Eva Reitzel <evareitzel@gmail.com>
2023-06-03 08:53:44 -0400

    Card component extracted

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,28 +1,29 @@
 import { useState } from "react"
 
 function Card ({ crypto }){
-  const [quantity, setQuantity] = useState(null) // FIX --> crypto.walletcryptos[0].quantity
-  
+  const [quantity, setQuantity] = useState(crypto.walletcryptos[0].quantity)
+
+  function handleSubmit(e){
+    setQuantity(e.target.value)
+  }
+
   return(
-
     <li key={crypto.id} className="card">
-      {/* <p>{crypto.crypto_id}</p> */}
-      {/* {console.log(crypto)} GET crypto.crypto */}
-      {console.log(crypto.walletcryptos[0].quantity)}
-
+      {/* {console.log(crypto.walletcryptos)} */}
       <h2>{crypto.name}</h2>
       <p><strong>{crypto.symbol}</strong> ${crypto.price}</p>
-      <form> 
-        {/* onSubmit={handleSubmit} */}
-        <label>Quantity</label>
-        <input 
-          className="form-input"
-          type='text'
-          // id=
-          value= {quantity} // {crypto.walletcryptos[0].quantity}
-          autoComplete='off'
-          onChange={e => setQuantity(e.target.value)}
-        />
+      <form onSubmit={handleSubmit} > 
+        <div className='form-field'>
+          <label>Quantity</label>
+          <input 
+            className="quantity-input"
+            type='text'
+            // id=
+            value= {quantity} // {crypto.walletcryptos[0].quantity}
+            autoComplete='off'
+            onChange={e => setQuantity(e.target.value)}
+          />
+        </div>
         <button type='submit' className='ghost-button'>Update Quantity</button>
       </form>
 
@@ -32,3 +33,28 @@ function Card ({ crypto }){
 }
 
 export default Card
+
+// onRemoveCrypto
+  //       <input 
+  //         className="form-input"
+  //         type='text'
+  //         autoComplete='off'
+  //         onChange={e => setWalletKey(e.target.value)}
+  //       />
+
+
+  // console.log(walletCryptos)
+
+
+  // function onIncrementClick(crypto){
+  //   setCryptoQuantity(crypto.quantity += 1)
+  //   onIncrementCrypto(crypto, cryptoQuantity)
+  // }
+
+
+  // const [cryptoQuantity, setCryptoQuantity] = useState(1)
+
+
+// <button className='incrementer' onClick={() => onIncrementClick(crypto)}>+</button> 
+// <button className='decrementer'>-</button>
+// onClick={onDecrementCryptos}
--- client/src/pages/Wallet.js
+++ client/src/pages/Wallet.js
@@ -1,42 +1,9 @@
-import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import Card from "../components/Card"
 
-
 function Wallet({ walletKey, cryptos }) {
-  const [quantity, setQuantity] = useState(null) // FIX --> crypto.walletcryptos[0].quantity
   const navigate = useNavigate()
-
-
-  const renderCryptos = cryptos.map(crypto => ( 
-    ////// make new card component so quantity state can be for ea individual
-    
-    <Card crypto={crypto} />
-
-    // <li key={crypto.id} className="card">
-    //   {/* <p>{crypto.crypto_id}</p> */}
-    //   {/* {console.log(crypto)} GET crypto.crypto */}
-    //   {console.log(crypto.walletcryptos[0].quantity)}
-
-    //   <h2>{crypto.name}</h2>
-    //   <p><strong>{crypto.symbol}</strong> ${crypto.price}</p>
-    //   <form> 
-    //     {/* onSubmit={handleSubmit} */}
-    //     <label>Quantity</label>
-    //     <input 
-    //       className="form-input"
-    //       type='text'
-    //       // id=
-    //       value= {quantity} // {crypto.walletcryptos[0].quantity}
-    //       autoComplete='off'
-    //       onChange={e => setQuantity(e.target.value)}
-    //     />
-    //     <button type='submit' className='ghost-button'>Update Quantity</button>
-    //   </form>
-
-    //   <h3>${crypto.walletcryptos[0].quantity * crypto.price}</h3>
-    // </li>
-  ))
+  const renderCryptos = cryptos.map(crypto => <Card crypto={crypto} />)
 
   return (
     <div>
@@ -52,29 +19,3 @@ function Wallet({ walletKey, cryptos }) {
 }
 
 export default Wallet
-
-
-  // onRemoveCrypto
-  //       <input 
-  //         className="form-input"
-  //         type='text'
-  //         autoComplete='off'
-  //         onChange={e => setWalletKey(e.target.value)}
-  //       />
-
-
-  // console.log(walletCryptos)
-
-
-  // function onIncrementClick(crypto){
-  //   setCryptoQuantity(crypto.quantity += 1)
-  //   onIncrementCrypto(crypto, cryptoQuantity)
-  // }
-
-
-  // const [cryptoQuantity, setCryptoQuantity] = useState(1)
-
-
-// <button className='incrementer' onClick={() => onIncrementClick(crypto)}>+</button> 
-// <button className='decrementer'>-</button>
-// onClick={onDecrementCryptos}
